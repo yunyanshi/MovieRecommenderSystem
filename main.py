b9073b7a7cf6402889e02f05e84e3a38979c426b
yunyanshi <ann.yunyanshi@gmail.com>
2021-11-21 21:45:09 -0800

    custome MAE:0.758249876867509

--- main.py
+++ main.py
@@ -283,7 +283,28 @@ def process_custom_v3(user_id, mean_ratings, std_ratings, rated_rating, rated_ra
         results.append("{} {} {}".format(user_id, predicted_mid + 1, rating))
     return results
 
-def process_custom_v4(user_id, mean_ratings, std_ratings, rated_rating, rated_rating_std, predicted, model):
+
+def process_custom_v4(user_id, mean_ratings, std_ratings, rated_rating, rated_rating_std, predicted, given_num):
+    mean_rating_of_active_user = np.mean(rated_rating)
+
+    results = []
+    for predicted_mid in predicted:
+        movie_rating_std = std_ratings[predicted_mid]
+        movie_rating = mean_ratings[predicted_mid]
+
+        if movie_rating_std <= 0.1:
+            rating = round(movie_rating)
+        elif movie_rating_std < 1.50 and rated_rating_std < 1.50:
+            rating = (1 / movie_rating_std * movie_rating + 1 / rated_rating_std * mean_rating_of_active_user) / (1 / movie_rating_std + 1 / rated_rating_std)
+            rating = round(rating)
+        elif movie_rating_std < rated_rating_std:
+            rating = round(movie_rating)
+        else:
+            rating = round(mean_rating_of_active_user)
+        results.append("{} {} {}".format(user_id, predicted_mid + 1, rating))
+    return results
+
+def process_custom_v100(user_id, mean_ratings, std_ratings, rated_rating, rated_rating_std, predicted, model):
     # 0.77286
     mean_rating_of_active_user = np.mean(rated_rating)
 
@@ -395,7 +416,7 @@ def predict_rating(training_data, test_data, neighbor_num, given_num, algorithm,
                                                     user_ratings_mean, algorithm, iuf, given_num)
             if algorithm is Algorithm.CUSTOM:
                 # results = results + process_custom(cur_user_id, movie_ratings_mean, movie_ratings_std, rating, predict_mid)
-                results = results + process_custom_v3(cur_user_id, movie_ratings_mean, movie_ratings_std, rating, rating_std,
+                results = results + process_custom_v4(cur_user_id, movie_ratings_mean, movie_ratings_std, rating, rating_std,
                                                       predict_mid, given_num)
             if algorithm is Algorithm.ITEM_BASED:
                 results = results + process_item_based_v2(cur_user_id, similarity, rated_mid, rating, predict_mid, given_num, neighbor_distribution)
